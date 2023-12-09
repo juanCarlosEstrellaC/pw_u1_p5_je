547a28e23ddc33537706e90ee383167d6fca104d
Juan Estrella <jcestrella@uce.edu.ec>
2023-12-09 10:37:18 -0500

    taller14, directivas en vue.js

--- js/app.js
+++ js/app.js
@@ -4,13 +4,35 @@ console.log(Vue);// Vue es un objeto
 // Lineas para configurar la app:
 const app = Vue.createApp({
     // No es buena práctica poner cod html aqui:
-   /*  template: `
-    <h1>Hola mundo</h1>
-    <p>Desde App.js</p>
-    <h6>{{1+1}}</h6>
-    <p>{{ [1,2,3,4,5,6,7,8] }}</p>
-    <p>{{ false?'Verdadero':'Falso'}}</p>
-    ` */
+    /*  template: `
+     <h1>Hola mundo</h1>
+     <p>Desde App.js</p>
+     <h6>{{1+1}}</h6>
+     <p>{{ [1,2,3,4,5,6,7,8] }}</p>
+     <p>{{ false?'Verdadero':'Falso'}}</p>
+     ` */
+    //Options API
+    methods: {
+        cambiarMensaje() {
+            console.log("Se está cambiando el mensaje.");
+            console.log(this.mensaje);
+            mensaje = "Valor cambiado"
+        },
+        cambiarNumero() {
+            this.valor = this.valor + 1;
+            console.log(this.valor);
+        }
+
+    },
+    watch: {
+
+    },
+    data() {
+        return {
+            mensaje: 'Hola mundo, desde Vue.js',
+            valor: 100
+        }
+    }
 
 })
 app.mount('#myApp') // le doy un id a mi aplicación
