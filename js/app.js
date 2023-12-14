f0bacda4fbef754224c2cba3cfe74f0aedb7227a
Juan Estrella <jcestrella@uce.edu.ec>
2023-12-13 19:46:57 -0500

    taller15

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 console.log("elementos de Vue");
 console.log(Vue);// Vue es un objeto
 
+const estudiantes = [{ nombre: 'Edison', apellido: 'Cayambe' },
+{ nombre: 'Daniel', apellido: 'Oviedo' },
+{ nombre: 'Karla', apellido: 'Perez' },
+{ nombre: 'Julio', apellido: 'Castillo' },
+{ nombre: 'Andrea', apellido: 'Teran' },
+]
+
+console.log(estudiantes);
+console.table(estudiantes)
+
 // Lineas para configurar la app:
 const app = Vue.createApp({
     // No es buena práctica poner cod html aqui:
@@ -21,6 +31,26 @@ const app = Vue.createApp({
         cambiarNumero() {
             this.valor = this.valor + 1;
             console.log(this.valor);
+        },
+        agregarEstudiante() {
+            console.log("Agregando estudiante.")
+            // debo añadir un objeto estudiante
+            const estu = { nombre: this.nombre, apellido: this.apellido }
+            // Unshift: agrega al inicio de la lista
+       /*      this.lista.unshift(estu) */
+            // Push: agrega al final de la lista
+            this.lista.push(estu)
+        },
+        presionandoTecla(event){ // event es un argumento por defecto, no es necesario ponerlo
+            console.log("presionando...");
+            console.log(event.charCode);
+        },
+        metodoApellido(event){
+            console.log(event.charCode);
+
+            if (event.charCode === 13) {
+                this.agregarEstudiante()
+            }
         }
 
     },
@@ -30,7 +60,10 @@ const app = Vue.createApp({
     data() {
         return {
             mensaje: 'Hola mundo, desde Vue.js',
-            valor: 100
+            valor: 100,
+            lista: estudiantes,
+            nombre: null,
+            apellido: null
         }
     }
 
