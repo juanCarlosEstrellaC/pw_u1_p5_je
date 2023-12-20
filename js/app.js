de37c4e3771dc90d3d8647987a929ab3e4cd7b9d
Juan Estrella <jcestrella@uce.edu.ec>
2023-12-20 13:44:40 -0500

    anotaciones en taller16

--- js/app.js
+++ js/app.js
@@ -1,32 +1,37 @@
-console.log("elementos de Vue");
-console.log(Vue);// Vue es un objeto
+// Imprimir por consola el objeto Vue.
+console.log("Imprimo los elementos del objeto Vue:");
+console.log(Vue);       // Vue es un objeto
 
-const estudiantes = [{ nombre: 'Edison', apellido: 'Cayambe' },
-{ nombre: 'Daniel', apellido: 'Oviedo' },
-{ nombre: 'Karla', apellido: 'Perez' },
-{ nombre: 'Julio', apellido: 'Castillo' },
-{ nombre: '', apellido: 'Teran' },
+// Array de objetos estudiantes:
+const estudiantes = [
+    { nombre: 'Edison', apellido: 'Cayambe' },
+    { nombre: 'Daniel', apellido: 'Oviedo' },
+    { nombre: 'Karla', apellido: 'Perez' },
+    { nombre: 'Julio', apellido: 'Castillo' },
+    { nombre: '', apellido: 'Teran' },
 ]
-
 console.log(estudiantes);
 console.table(estudiantes)
 
-// Lineas para configurar la app:
+// CREAR una instancia de Vue: 
+// ""app" es una instancia de Vue. Luego, usando mount, se monta en el DOM.
 const app = Vue.createApp({
-    // No es buena práctica poner cod html aqui:
+    // No es buena práctica poner codigo HTML aquÍ:
     /*  template: `
      <h1>Hola mundo</h1>
      <p>Desde App.js</p>
      <h6>{{1+1}}</h6>
      <p>{{ [1,2,3,4,5,6,7,8] }}</p>
      <p>{{ false?'Verdadero':'Falso'}}</p>
-     ` */
-    //Options API
+     ` 
+     */
+
     methods: {
         cambiarMensaje() {
+            console.log("Mensaje actual: " + this.mensaje);
             console.log("Se está cambiando el mensaje.");
-            console.log(this.mensaje);
-            mensaje = "Valor cambiado"
+            this.mensaje = "Valor cambiado"
+            console.log("Nuevo Mensaje: " + this.mensaje);
         },
         cambiarNumero() {
             this.valor = this.valor + 1;
@@ -34,29 +39,24 @@ const app = Vue.createApp({
         },
         agregarEstudiante() {
             console.log("Agregando estudiante.")
-            // debo añadir un objeto estudiante
-            const estu = { nombre: this.nombre, apellido: this.apellido }
-            // Unshift: agrega al inicio de la lista
-       /*      this.lista.unshift(estu) */
-            // Push: agrega al final de la lista
-            this.lista.push(estu)
+            const estu = { nombre: this.nombre, apellido: this.apellido }    // Añadado un objeto estudiante:
+            this.lista.push(estu)       // Push añade un elemento al final del array y Unshift al inicio.
         },
-        presionandoTecla({charCode}){ // event es un argumento por defecto, no es necesario ponerlo
+        presionandoTecla({ charCode }) { // event es un argumento por defecto, no es necesario ponerlo
             console.log("presionando...");
             console.log(charCode);
         },
-        agregarEstudianteMod(){ // event es un argumento por defecto, no es necesario ponerlo
+        agregarEstudianteMod() { // event es un argumento por defecto, no es necesario ponerlo
             console.log(nombre);
             console.log("presionando 2...");
         },
-        metodoApellido(event){
+        metodoApellido(event) {  // event es un argumento por defecto, no es necesario ponerlo
             console.log(event.charCode);
 
             if (event.charCode === 13) {
                 this.agregarEstudiante()
             }
         }
-
     },
     watch: {
 
@@ -70,8 +70,6 @@ const app = Vue.createApp({
             apellido: undefined
         }
     }
-
 })
-app.mount('#myApp') // le doy un id a mi aplicación
 
-// Definición de estructuras de la app
+app.mount('#myApp') // Monto en el DOM a "app", y le doy un id a mi aplicación
